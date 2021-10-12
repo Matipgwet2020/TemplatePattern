841d0e06a88b9bfc17cb526d7751c2f2231c6f9b
mmatipgwet@gmail.com <mmatipgwet@gmail.com>
2021-10-12 11:36:56 +0100

    create to abstract class CafeineBeverage

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 "# TemplatePattern" 
+
+Baristas ! Please follow these recipes 
+precisely when preraring starbuzz beverages
+
+*Starbuzz coffee Recipe
+    1- Boil some water
+    2- Brew coffee in boiling water
+    3- Pour coffée in cup
+    4- Add sugar and milk
+
+* Starbuzz Tea Recipe
+    1-Boil Some water
+    2-Steep tea in boiling water
+    3-Pour tea in cup
+    4-Add lemon
+
+* We use TemplatePattern to represent this situation
+
---
+++ src/templatePattern/CaffeineBeverage.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templatePattern;
+
+/**
+ *
+ * @author Mgms
+ */
+public abstract class CaffeineBeverage {
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourIncup();
+        addCondiments();
+    }
+    
+    abstract void brew();
+    abstract void addCondiments();
+    
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+    
+    void pourIncup() {
+        System.out.println("Pouring into cup");
+    }
+}
