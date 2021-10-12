1e942ccca4a1d8f48667d0fd010654d1fd6f4442
mmatipgwet@gmail.com <mmatipgwet@gmail.com>
2021-10-12 11:39:57 +0100

    create to class Tea

---
+++ src/templatePattern/Tea.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templatePattern;
+
+/**
+ *
+ * @author Mgms
+ */
+public class Tea extends CaffeineBeverage {
+    
+    public void brew() {
+        System.out.println("Steeping the tea");
+    }
+    
+    public void addCondiments() {
+        System.out.println("Adding Lemon");
+    }
+    
+}
